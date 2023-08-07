0787b49a8c8634b11ddee3659c1e52f372098e36
sunaookamishirokodev <lethanhtrung.trungle@gmail.com>
2023-08-07 07:26:49 +0700

    website

---
+++ src/web/public/assets/js/index.js
@@ -0,0 +1,31 @@
+(function() {
+    const processList = document.querySelector('.processList')
+    const performList = document.querySelector('.performList')
+    const leftProfileContainer = document.querySelector('.container-left')
+    const rightProfileContainer = document.querySelector('.container-right')
+    
+    const performtems = processList.querySelectorAll('.process-box__bar--percent')
+    const performItems = performList.querySelectorAll('.process-box__bar--percent')
+    const leftItems = leftProfileContainer.querySelectorAll('.container-box')
+    const rightItems = rightProfileContainer.querySelectorAll('.container-box')
+
+    function addObserver(items) {
+        for (const item of items) {
+            const observer = new IntersectionObserver(
+                (entries, observer) => entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setTimeout(() => {
+                            entry.target.classList.add('active-IO')
+                            observer.unobserve(entry.target)
+                        }, 300)
+                    }
+                })
+            )
+    
+            observer.observe(item)
+        }
+    }
+
+    const observerItems = [performtems, performItems, leftItems, rightItems]
+    observerItems.forEach(items => addObserver(items))
+})()
